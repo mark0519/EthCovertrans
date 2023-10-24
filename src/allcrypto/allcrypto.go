e9fe0f658c0e26fa383ffb3f3747f473dd472ecf
Mark <2578653827@QQ.com>
2023-10-24 23:19:00 +0800

    v1.0

--- src/allcrypto/allcrypto.go
+++ src/allcrypto/allcrypto.go
@@ -1,138 +1,130 @@
 package allcrypto
 
-import (
-	"crypto/ecdh"
-	"crypto/rand"
-	"crypto/sha256"
-	"fmt"
-	"golang.org/x/crypto/pbkdf2"
-)
-
-func TestEcdh() {
-	// ECDH（Elliptic Curve Diffie-Hellman）算法是一种基于椭圆曲线的密钥交换协议
-	// aliceKey和bobKey都是各自的私钥
-	// alicePubkey和bobPubkey是各自的公钥
-	// aliceShared和bobShared一致，为计算出的共享密钥（项目中的主密钥）
-	fmt.Println("[Test ECDH]=================================")
-	aliceKey, err := ecdh.P256().GenerateKey(rand.Reader)
-	if err != nil {
-		panic(err)
-	}
-	bobKey, err := ecdh.P256().GenerateKey(rand.Reader)
-	if err != nil {
-		panic(err)
-	}
-	alicePubkey := aliceKey.PublicKey()
-	shared, _ := bobKey.ECDH(alicePubkey)
-	bobShared := sha256.Sum256(shared)
-	fmt.Printf("秘钥哈希(Bob)  %x\n", bobShared)
-	// 秘钥哈希(Bob)  a74e7949e71ead5f3bd4de031e2ad45c3f5b80b48ccf50e50eb86f4bdb025c3a
-	bobPubkey := bobKey.PublicKey()
-	shared, _ = aliceKey.ECDH(bobPubkey)
-	aliceShared := sha256.Sum256(shared)
-	fmt.Printf("秘钥哈希(Alice)  %x\n", aliceShared)
-	// 秘钥哈希(Alice)  a74e7949e71ead5f3bd4de031e2ad45c3f5b80b48ccf50e50eb86f4bdb025c3a
-
-}
-
-func TestEcdhExchange() [32]byte {
-	fmt.Println("[Test Ecdh Key Exchange]=================================")
-	// Alice和Bob 公私钥对生成
-	aliceKey, err := ecdh.P256().GenerateKey(rand.Reader)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Alice私钥： %x\n", aliceKey.Bytes())
-	alicePubkey := aliceKey.PublicKey()
-	fmt.Printf("Alice公钥： %x\n", alicePubkey.Bytes())
-
-	bobKey, err := ecdh.P256().GenerateKey(rand.Reader)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Bob私钥： %x\n", bobKey.Bytes())
-	bobPubkey := bobKey.PublicKey()
-	fmt.Printf("Bob公钥： %x\n", bobPubkey.Bytes())
-
-	// 密钥交换：双方交换公钥，Bob获得alicePubkey，Alice获得bobPubkey
-
-	// Bob利用自己的私钥（Bob私钥）和Alice公钥计算共享主密钥mainKey
-	shared, _ := bobKey.ECDH(alicePubkey)
-	bobShared := sha256.Sum256(shared)
-	fmt.Printf("Bob计算出的共享主密钥mainKey  %x\n", bobShared)
-
-	// Alice利用自己的私钥（Alice私钥）和Bob公钥计算共享主密钥mainKey
-	shared, _ = aliceKey.ECDH(bobPubkey)
-	aliceShared := sha256.Sum256(shared)
-	fmt.Printf("Alice计算出的共享主密钥mainKey  %x\n", aliceShared)
-
-	// 返回mainKey
-	return aliceShared
-}
-
-func GetMainKey() [32]byte {
-	// 获得主密钥（链下）
-
-	// Alice和Bob 公私钥对生成
-	aliceKey, err := ecdh.P256().GenerateKey(rand.Reader)
-	if err != nil {
-		panic(err)
-	}
-	//fmt.Printf("Alice私钥： %x\n", aliceKey.Bytes())
-	alicePubkey := aliceKey.PublicKey()
-	//fmt.Printf("Alice公钥： %x\n", alicePubkey.Bytes())
-
-	bobKey, err := ecdh.P256().GenerateKey(rand.Reader)
-	if err != nil {
-		panic(err)
-	}
-	//fmt.Printf("Bob私钥： %x\n", bobKey.Bytes())
-	bobPubkey := bobKey.PublicKey()
-	//fmt.Printf("Bob公钥： %x\n", bobPubkey.Bytes())
-
-	// 密钥交换：双方交换公钥，Bob获得alicePubkey，Alice获得bobPubkey
-
-	// Bob利用自己的私钥（Bob私钥）和Alice公钥计算共享主密钥mainKey
-	shared, _ := bobKey.ECDH(alicePubkey)
-	bobShared := sha256.Sum256(shared)
-	//fmt.Printf("Bob计算出的共享主密钥mainKey  %x\n", bobShared)
-
-	// Alice利用自己的私钥（Alice私钥）和Bob公钥计算共享主密钥mainKey
-	shared, _ = aliceKey.ECDH(bobPubkey)
-	aliceShared := sha256.Sum256(shared)
-	//fmt.Printf("Alice计算出的共享主密钥mainKey  %x\n", aliceShared)
-
-	// 返回mainKey
-	if bobShared == aliceShared {
-		return aliceShared
-	} else {
-		panic("密钥计算错误")
-	}
-}
-
-func GetTransKey(mainKey [32]byte, transactionHash string) []byte {
-	// 使用pbkdf2密钥派生算法计算每次的通信密钥tansKey
-
-	// transactionHash就是上次交易的Transaction Hash
-	// transactionHash因该是字符串类型，形如：
-	// '0x8879c312dd900030f514b7fe44abcf64bf7c825c02b9a0a9e3bbc723bab2d004'
-	if len(transactionHash) != 66 {
-		panic("Transaction Hash错误")
-	}
-	// token 为transactionHash的后16位
-	token := transactionHash[len(transactionHash)-16:]
-
-	newToken := make([]byte, len(token))
-	copy(newToken, token)
-	newMainKey := mainKey[:]
-	// pbkdf2密钥派生算法，迭代次数4，迭代方式Sha256,得到每次交易密钥长度32
-	transKey := pbkdf2.Key(newMainKey, newToken, 4, 32, sha256.New)
-	return transKey
-}
-
-func getMainKey() {
-	/*
-
-	 */
-
-}
+//func TestEcdh() {
+//	// ECDH（Elliptic Curve Diffie-Hellman）算法是一种基于椭圆曲线的密钥交换协议
+//	// aliceKey和bobKey都是各自的私钥
+//	// alicePubkey和bobPubkey是各自的公钥
+//	// aliceShared和bobShared一致，为计算出的共享密钥（项目中的主密钥）
+//	fmt.Println("[Test ECDH]=================================")
+//	aliceKey, err := ecdh.P256().GenerateKey(rand.Reader)
+//	if err != nil {
+//		panic(err)
+//	}
+//	bobKey, err := ecdh.P256().GenerateKey(rand.Reader)
+//	if err != nil {
+//		panic(err)
+//	}
+//	alicePubkey := aliceKey.PublicKey()
+//	shared, _ := bobKey.ECDH(alicePubkey)
+//	bobShared := sha256.Sum256(shared)
+//	fmt.Printf("秘钥哈希(Bob)  %x\n", bobShared)
+//	// 秘钥哈希(Bob)  a74e7949e71ead5f3bd4de031e2ad45c3f5b80b48ccf50e50eb86f4bdb025c3a
+//	bobPubkey := bobKey.PublicKey()
+//	shared, _ = aliceKey.ECDH(bobPubkey)
+//	aliceShared := sha256.Sum256(shared)
+//	fmt.Printf("秘钥哈希(Alice)  %x\n", aliceShared)
+//	// 秘钥哈希(Alice)  a74e7949e71ead5f3bd4de031e2ad45c3f5b80b48ccf50e50eb86f4bdb025c3a
+//
+//}
+//
+//func TestEcdhExchange() [32]byte {
+//	fmt.Println("[Test Ecdh Key Exchange]=================================")
+//	// Alice和Bob 公私钥对生成
+//	aliceKey, err := ecdh.P256().GenerateKey(rand.Reader)
+//	if err != nil {
+//		panic(err)
+//	}
+//	fmt.Printf("Alice私钥： %x\n", aliceKey.Bytes())
+//	alicePubkey := aliceKey.PublicKey()
+//	fmt.Printf("Alice公钥： %x\n", alicePubkey.Bytes())
+//
+//	bobKey, err := ecdh.P256().GenerateKey(rand.Reader)
+//	if err != nil {
+//		panic(err)
+//	}
+//	fmt.Printf("Bob私钥： %x\n", bobKey.Bytes())
+//	bobPubkey := bobKey.PublicKey()
+//	fmt.Printf("Bob公钥： %x\n", bobPubkey.Bytes())
+//
+//	// 密钥交换：双方交换公钥，Bob获得alicePubkey，Alice获得bobPubkey
+//
+//	// Bob利用自己的私钥（Bob私钥）和Alice公钥计算共享主密钥mainKey
+//	shared, _ := bobKey.ECDH(alicePubkey)
+//	bobShared := sha256.Sum256(shared)
+//	fmt.Printf("Bob计算出的共享主密钥mainKey  %x\n", bobShared)
+//
+//	// Alice利用自己的私钥（Alice私钥）和Bob公钥计算共享主密钥mainKey
+//	shared, _ = aliceKey.ECDH(bobPubkey)
+//	aliceShared := sha256.Sum256(shared)
+//	fmt.Printf("Alice计算出的共享主密钥mainKey  %x\n", aliceShared)
+//
+//	// 返回mainKey
+//	return aliceShared
+//}
+//
+//func GetMainKey() [32]byte {
+//	// 获得主密钥（链下）
+//
+//	// Alice和Bob 公私钥对生成
+//	aliceKey, err := ecdh.P256().GenerateKey(rand.Reader)
+//	if err != nil {
+//		panic(err)
+//	}
+//	//fmt.Printf("Alice私钥： %x\n", aliceKey.Bytes())
+//	alicePubkey := aliceKey.PublicKey()
+//	//fmt.Printf("Alice公钥： %x\n", alicePubkey.Bytes())
+//
+//	bobKey, err := ecdh.P256().GenerateKey(rand.Reader)
+//	if err != nil {
+//		panic(err)
+//	}
+//	//fmt.Printf("Bob私钥： %x\n", bobKey.Bytes())
+//	bobPubkey := bobKey.PublicKey()
+//	//fmt.Printf("Bob公钥： %x\n", bobPubkey.Bytes())
+//
+//	// 密钥交换：双方交换公钥，Bob获得alicePubkey，Alice获得bobPubkey
+//
+//	// Bob利用自己的私钥（Bob私钥）和Alice公钥计算共享主密钥mainKey
+//	shared, _ := bobKey.ECDH(alicePubkey)
+//	bobShared := sha256.Sum256(shared)
+//	//fmt.Printf("Bob计算出的共享主密钥mainKey  %x\n", bobShared)
+//
+//	// Alice利用自己的私钥（Alice私钥）和Bob公钥计算共享主密钥mainKey
+//	shared, _ = aliceKey.ECDH(bobPubkey)
+//	aliceShared := sha256.Sum256(shared)
+//	//fmt.Printf("Alice计算出的共享主密钥mainKey  %x\n", aliceShared)
+//
+//	// 返回mainKey
+//	if bobShared == aliceShared {
+//		return aliceShared
+//	} else {
+//		panic("密钥计算错误")
+//	}
+//}
+//
+//func GetTransKey(mainKey [32]byte, transactionHash string) []byte {
+//	// 使用pbkdf2密钥派生算法计算每次的通信密钥tansKey
+//
+//	// transactionHash就是上次交易的Transaction Hash
+//	// transactionHash因该是字符串类型，形如：
+//	// '0x8879c312dd900030f514b7fe44abcf64bf7c825c02b9a0a9e3bbc723bab2d004'
+//	if len(transactionHash) != 66 {
+//		panic("Transaction Hash错误")
+//	}
+//	// token 为transactionHash的后16位
+//	token := transactionHash[len(transactionHash)-16:]
+//
+//	newToken := make([]byte, len(token))
+//	copy(newToken, token)
+//	newMainKey := mainKey[:]
+//	// pbkdf2密钥派生算法，迭代次数4，迭代方式Sha256,得到每次交易密钥长度32
+//	transKey := pbkdf2.Key(newMainKey, newToken, 4, 32, sha256.New)
+//	return transKey
+//}
+//
+//func getMainKey() {
+//	/*
+//
+//	 */
+//
+//}
--- src/main.go
+++ src/main.go
@@ -3,36 +3,48 @@ package main
 import (
 	"EthCovertrans/src/allcrypto"
 	"context"
-	"crypto/ecdsa"
+	"encoding/hex"
 	"fmt"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
-	"math/big"
 )
 
+type sharedData struct {
+	pubKeyStr string
+	psk       string
+	n         int
+}
+
 func main() {
-	client := initEthClient()
-	testGetBalance(client) // 检查和ETH网关（gateway）的连接
-	//key := testGetKey()
-	//addr, _ := createMsgAccount(key)
-	//fmt.Println("钱包地址：" + addr.Hex())
+	//client := initEthClient()
+	//testGetBalance(client) // 检查和ETH网关（gateway）的连接
+
+	sharedData := sharedData{
+		pubKeyStr: "04023b1d8cfbdfe2c5fb8cf1623bb5766c57c8458bad2f6ab2f4cecd70ad682b9de61c22438917d9ee2059f84b604b982ed375b596f3940461076851b60e0a191e",
+		psk:       "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+		n:         2,
+	}
+
+	sender(sharedData)
 
 }
 
-func testGetKey() []byte {
-	mainKey := allcrypto.GetMainKey()
-	fmt.Printf("共享主密钥(MainKey): %x\n", mainKey)
-	transHash := "0x8879c312dd900030f514b7fe44abcf64bf7c825c02b9a0a9e3bbc723bab2d004"
-	key := allcrypto.GetTransKey(mainKey, transHash)
-	fmt.Printf("通信私钥：: %x\n", key)
-	return key
-}
-
-func getKeyByToken(mainKey [32]byte, transHash string) []byte {
-	key := allcrypto.GetTransKey(mainKey, transHash)
-	return key
+func sender(data sharedData) {
+	// Private Key: 0x93d5d04256882aaad507ff09f510969f347758109793448aa79e1b4dbe5f6efa
+	// Public Key : 0x04023b1d8cfbdfe2c5fb8cf1623bb5766c57c8458bad2f6ab2f4cecd70ad682b9de61c22438917d9ee2059f84b604b982ed375b596f3940461076851b60e0a191e
+	// Address    : 0xa4528e245F87CBA1D650403d196eF505EE4D0a2B
+	PrivateKeyStr := "93d5d04256882aaad507ff09f510969f347758109793448aa79e1b4dbe5f6efa"
+	sk, _ := hex.DecodeString(PrivateKeyStr)
+	pskStr := data.psk
+	psk, _ := hex.DecodeString(pskStr)
+	n := data.n // 默认为2
+	addrTable := allcrypto.InitAddrTable(n)
+	times := 20
+	// 预先计算20个地址
+	key := allcrypto.SetAddrDatas(addrTable, sk, psk, n, times)
+	allcrypto.ShowAddrTable(addrTable)
+	fmt.Printf("Netx key: %x\n", key)
 }
 
 func initEthClient() *ethclient.Client {
@@ -55,17 +67,68 @@ func testGetBalance(c *ethclient.Client) {
 	fmt.Printf("Balance: %d wei\n", balance)
 }
 
-func createMsgAccount(key []byte) (common.Address, ecdsa.PublicKey) {
-	// 假设你有一个 Ethereum 私钥 key,用来他生成公钥并计算钱包地址
-	//fmt.Println("私钥：" + hexutil.Encode(key))
-	privateKeyBytes := key
-	privateKey := new(ecdsa.PrivateKey)
-	privateKey.D = new(big.Int).SetBytes(privateKeyBytes)
-	privateKey.PublicKey.Curve = crypto.S256()
-	privateKey.PublicKey.X, privateKey.PublicKey.Y = privateKey.PublicKey.Curve.ScalarBaseMult(privateKey.D.Bytes())
-	publicKey := privateKey.PublicKey
-	// 获取公钥地址
-	address := crypto.PubkeyToAddress(privateKey.PublicKey)
-	//fmt.Println("钱包地址：" + address.Hex())
-	return address, publicKey
-}
+//func createMsgAccount(key []byte) (common.Address, ecdsa.PublicKey) {
+//	// 假设你有一个 Ethereum 私钥 key,用来他生成公钥并计算钱包地址
+//	//fmt.Println("私钥：" + hexutil.Encode(key))
+//	privateKeyBytes := key
+//	privateKey := new(ecdsa.PrivateKey)
+//	privateKey.D = new(big.Int).SetBytes(privateKeyBytes)
+//	privateKey.PublicKey.Curve = crypto.S256()
+//	privateKey.PublicKey.X, privateKey.PublicKey.Y = privateKey.PublicKey.Curve.ScalarBaseMult(privateKey.D.Bytes())
+//	publicKey := privateKey.PublicKey
+//	// 获取公钥地址
+//	address := crypto.PubkeyToAddress(privateKey.PublicKey)
+//	//fmt.Println("钱包地址：" + address.Hex())
+//
+//	//crypto.
+//
+//	return address, publicKey
+//}
+
+//func testGetKey() []byte {
+//	mainKey := allcrypto.GetMainKey()
+//	fmt.Printf("共享主密钥(MainKey): %x\n", mainKey)
+//	transHash := "0x8879c312dd900030f514b7fe44abcf64bf7c825c02b9a0a9e3bbc723bab2d004"
+//	key := allcrypto.GetTransKey(mainKey, transHash)
+//	fmt.Printf("通信私钥：: %x\n", key)
+//	return key
+//}
+//
+//func getKeyByToken(mainKey [32]byte, transHash string) []byte {
+//	key := allcrypto.GetTransKey(mainKey, transHash)
+//	return key
+//}
+//
+//func test() {
+//	// 生成一个私钥
+//	//privateKey, err := ecdsa.GenerateKey(secp256k1.S256(), rand.Reader)
+//	//if err != nil {
+//	//	log.Fatal(err)
+//	//}
+//
+//	// 生成一个随机数作为标量 k
+//	k1, err := rand.Int(rand.Reader, secp256k1.S256().Params().N)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	k2, err := rand.Int(rand.Reader, secp256k1.S256().Params().N)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	k := new(big.Int).Mul(k1, k2)
+//	k = k.Mod(k, secp256k1.S256().Params().N)
+//
+//	// 计算 psk*k
+//	x, y := secp256k1.S256().ScalarBaseMult(k1.Bytes())
+//	//x, y := secp256k1.S256().ScalarMult(big.NewInt(1), big.NewInt(2), k1.Bytes())
+//	x1, y1 := secp256k1.S256().ScalarMult(x, y, k2.Bytes())
+//
+//	x2, y2 := secp256k1.S256().ScalarBaseMult(k.Bytes())
+//
+//	fmt.Println("psk*sk:")
+//	fmt.Println("x:", x1)
+//	fmt.Println("y:", y1)
+//	fmt.Println("x:", x2)
+//	fmt.Println("y:", y2)
+//
+//}
